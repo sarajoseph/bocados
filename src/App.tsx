6f1251ded974f8e583b2ab1369c19682b106bab0
sarajoseph <sarajosephperez@gmail.com>
2025-03-13 15:13:30 +0100

    scroll to top all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
 import { Home } from '@pages/Home'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
 import { NotFound } from '@pages/NotFound'
 import { Menu } from '@pages/Menu'
 import { Contact } from '@pages/Contact'
+import { ScrollToTop } from '@components/common/ScrollToTop'
+
+const Layout = () => (
+  <>
+    <ScrollToTop />
+    <Outlet />
+  </>
+)
 
 export const App = () => {
-
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <Home />,
-      errorElement: <NotFound />
-    },
-    {
-      path: '/menu',
-      element: <Menu />,
-      errorElement: <NotFound />
-    },
-    {
-      path: '/contact',
-      element: <Contact />,
-      errorElement: <NotFound />
-    },
+      element: <Layout />,
+      children: [
+        { path: '/', element: <Home />, errorElement: <NotFound /> },
+        { path: '/menu', element: <Menu />, errorElement: <NotFound /> },
+        { path: '/contact', element: <Contact />, errorElement: <NotFound /> },
+        { path: '*', element: <NotFound /> }
+      ]
+    }
   ])
   return <RouterProvider router={router} />
 }
